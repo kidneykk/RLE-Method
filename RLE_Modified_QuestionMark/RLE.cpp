36dea91a2cbf7a62b5eb46bb274490bf2a79e3f1
kidney <62258499+kidneykk@users.noreply.github.com>
2020-08-08 17:39:38 +0300

    Update RLE.cpp

--- RLE_Modified_QuestionMark/RLE.cpp
+++ RLE_Modified_QuestionMark/RLE.cpp
@@ -25,10 +25,10 @@ void RLE::encode(const std::string& filename) {
 	m_initialSize = fin.tellg();	// get input file size
 	fin.seekg(0, std::ios::beg);
 
-	std::string e_filename{ filename };					// encoded filename
+	std::string e_filename{ filename };			// encoded filename
 	if (e_filename.find(".rle") == std::string::npos)	// if there is no ".rle" extension
-		e_filename.append(".rle");						// add ".rle" to the end
-	else {												// otherwise throw error
+		e_filename.append(".rle");			// add ".rle" to the end
+	else {							// otherwise throw error
 		fin.close();
 		throw error_extension(false);
 	}
@@ -38,14 +38,14 @@ void RLE::encode(const std::string& filename) {
 	unsigned int count{ 1 };		// number of equal characters
 	while (!fin.eof()) {
 		fin.read(m_block.data(), m_block.size() - 1);	// read block from file to vector
-		std::streamsize dataSize = fin.gcount();		// get the number of characters by the last read operation
+		std::streamsize dataSize = fin.gcount();	// get the number of characters by the last read operation
 
 		// Encoding
 		for (int i = 0; i < dataSize; ++i) {
 			if (m_block[i] == m_block[i + 1]) {			// if the next char equals current
-				count++;								// count it
+				count++;					// count it
 			}
-			else {										// another char
+			else {							// another char
 				// create a string with the following structure:
 				// number_of_equal_chars|current_char
 				std::string temp = std::to_string(count) + '|' + m_block[i];
@@ -68,13 +68,13 @@ void RLE::decode(const std::string& filename) {
 		throw error_file(filename);
 	}
 
-	std::string d_filename{ filename };					// decoded filename
+	std::string d_filename{ filename };			// decoded filename
 
 	// all encoded files must contain ".rle" extension!
 	std::size_t fileExt_pos = d_filename.find(".rle");	// try to find extension
-	if (fileExt_pos != std::string::npos)				// if ".rle" does exist
-		d_filename.erase(fileExt_pos, 4);				// erase it
-	else {												// otherwise throw error
+	if (fileExt_pos != std::string::npos)			// if ".rle" does exist
+		d_filename.erase(fileExt_pos, 4);		// erase it
+	else {							// otherwise throw error
 		fin.close();
 		throw error_extension(true);
 	}
@@ -86,22 +86,22 @@ void RLE::decode(const std::string& filename) {
 		std::streamsize dataSize = fin.gcount();		// get the number of characters by the last read operation
 		if (!dataSize) break;
 
-		int offset{ 0 };								// used for correct input
+		int offset{ 0 };					// used for correct input
 		while (m_block[dataSize - 2 + offset] != '|')
 			offset--;
 		fin.seekg(offset, fin.cur);
 
-		unsigned int count{ 0 };						// number of equal chars
-		std::stringstream ss;							// for convertion
+		unsigned int count{ 0 };				// number of equal chars
+		std::stringstream ss;					// for convertion
 
 		for (int i = 0; i < dataSize + offset - 1; ++i) {
-			if (std::isdigit(m_block[i])) {				// if current char is digit
-				ss << m_block[i];						// push it to stringstream
+			if (std::isdigit(m_block[i])) {			// if current char is digit
+				ss << m_block[i];			// push it to stringstream
 			}
-			else {										// current char == '|'
-				ss >> count;							// convert numbers
+			else {						// current char == '|'
+				ss >> count;				// convert numbers
 				for (int j = 0; j < count; ++j)
-					fout.put(m_block[i + 1]);			// write char to the output file
+					fout.put(m_block[i + 1]);	// write char to the output file
 				count = 0;
 				ss.str("");
 				ss.clear();
